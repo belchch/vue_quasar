59c94dea5309da2f9eb46f0d3cf48754a10e677c
belchch <rbelchenko@gmail.com>
2025-08-18 17:24:01 +0300

    dev-110 refactor floor

---
+++ src/features/boq/components/configuration/ReplacementToggles.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="row q-gutter-md">
+        <q-toggle color="secondary" v-model="replacement" label="Замена" size="sm"/>
+
+        <q-toggle color="secondary" v-model="preservation" label="С сохранением" size="sm" :disable="!replacement" />
+    </div>
+</template>
+<script lang="ts" setup>
+const replacement = defineModel<boolean>('replacement', { required: true })
+const preservation = defineModel<boolean>('preservation', { required: true })
+
+</script>
