3909bafab3c17e896cea9d2775d24a77c8b7a60c
MaxD <controldms@gmail.com>
2025-10-08 17:38:17 +0300

    sticy header

--- src/features/estimate/components/EstimateTable.vue
+++ src/features/estimate/components/EstimateTable.vue
@@ -1,81 +1,82 @@
 <template>
   <div class="q-pt-sm">
-    <q-table :rows="estimateStore.estimate?.groups || []" :rows-per-page-options="[0]" :row-key="(row) => getKey(row)"
-      hide-header hide-bottom hide-pagination bordered wrap-cells>
-      <template v-slot:top-row>
-        <q-tr>
-          <q-td colspan="100%" style="text-align: center;">
-            <div>СМЕТА</div>
-            <div>на выполнение строительно-ремонтных работ</div>
-          </q-td>
-        </q-tr>
-      </template>
-      <template v-slot:body="props">
-        <q-tr :props="props" @click="props.expand = !props.expand">
-          <q-td class="second-row">
-            <span>{{ props.row.description }}</span>
-          </q-td>
-          <q-td auto-width style="text-align: right;">
-            <q-icon :name="props.expand ? 'keyboard_arrow_up' : 'keyboard_arrow_down'"
-              @click.stop="props.expand = !props.expand" size="sm" color="grey-7" />
-          </q-td>
-        </q-tr>
-        <q-tr v-show="props.expand" :props="props" no-hover>
-          <q-td colspan="100%">
-            <estimate-table-group :total="props.row.total" :works="props.row.works" />
-          </q-td>
-        </q-tr>
-      </template>
-      <template #bottom-row>
-        <q-tr class="main-row">
-          <q-td>
-            Итого, руб.
-          </q-td>
-          <q-td>{{ estimateStore.estimate?.worksTotal }}</q-td>
-        </q-tr>
-        <q-tr class="main-row">
-          <q-td>
-            Непредвиденные расходы (2 % от итоговой суммы), руб.
-          </q-td>
-          <q-td>{{ estimateStore.estimate?.unexpectedExpenses }}</q-td>
-        </q-tr>
-        <q-tr class="main-row">
-          <q-td>
-            Вывоз мусора (5% от итоговой суммы), руб
-          </q-td>
-          <q-td>{{ estimateStore.estimate?.garbageRemoval }}</q-td>
-        </q-tr>
-        <q-tr class="main-row">
-          <q-td>
-            Транспортные расходы (5% от итоговой суммы), руб
-          </q-td>
-          <q-td>{{ estimateStore.estimate?.transportation }}</q-td>
-        </q-tr>
-        <q-tr class="main-row">
-          <q-td>
-            Итого по всем разделам с непредвиденными расходами, руб.
-          </q-td>
-          <q-td>{{ estimateStore.estimate?.total }}</q-td>
-        </q-tr>
-      </template>
-    </q-table>
+    <q-markup-table bordered wrap-cells separator="cell" class="sticky-header-table" dense>
+      <caption class="q-pt-sm">
+        <div>СМЕТА</div>
+        <div>на выполнение строительно-ремонтных работ</div>
+      </caption>
+      <thead>
+        <tr>
+          <th class="text-left">Наименование</th>
+          <th class="text-right">Ед.изм.</th>
+          <th class="text-right">Объем</th>
+          <th class="text-right">Поставщик</th>
+          <th class="text-right">Цена услуги (руб.)</th>
+          <th class="text-right">Средняя цена (руб.)</th>
+          <th class="text-right">Средняя рыночная стоимость (руб.)</th>
+        </tr>
+      </thead>
+      <tbody>
+        <template v-for="(group, i) in estimateStore.estimate?.groups" :key="i">
+          <tr>
+            <td :colspan="7">{{ group.description }}</td>
+          </tr>
+          <tr v-for="(work, j) in group.works" :key="j">
+            <td>{{ work.name }}</td>
+            <td>{{ work.uom }}</td>
+            <td>{{ work.volume }}</td>
+            <td>
+              <template v-for="rate in work.rates" :key="rate">
+                <div><a :href="rate.url" target="_blank">{{ rate.url }}</a></div>
+              </template>
+            </td>
+            <td>
+              <template v-for="rate in work.rates" :key="rate">
+                <div>{{ rate.price }}</div>
+              </template>
+            </td>
+            <td>{{ work.averagePrice }}</td>
+            <td>{{ work.averageCost }}</td>
+          </tr>
+          <tr>
+            <td colspan="6">
+              Итого по разделу, руб.
+            </td>
+            <td>{{ group.total }}</td>
+          </tr>
+        </template>
+        <tr>
+          <td colspan="6">Итого, руб.</td>
+          <td>{{estimateStore.estimate?.worksTotal}}</td>
+        </tr>
+        <tr>
+          <td colspan="6">Непредвиденные расходы (2 % от итоговой суммы), руб.</td>
+          <td>{{ estimateStore.estimate?.unexpectedExpenses }}</td>
+        </tr>
+        <tr>
+          <td colspan="6">Вывоз мусора (5% от итоговой суммы), руб</td>
+          <td>{{ estimateStore.estimate?.garbageRemoval }}</td>
+        </tr>
+        <tr>
+          <td colspan="6">Транспортные расходы (5% от итоговой суммы), руб</td>
+          <td>{{ estimateStore.estimate?.transportation }}</td>
+        </tr>
+        <tr>
+          <td colspan="6">Итого по всем разделам с непредвиденными расходами, руб.</td>
+          <td>{{ estimateStore.estimate?.total }}</td>
+        </tr>
+      </tbody>
+    </q-markup-table>
   </div>
 </template>
 
 <script setup lang="ts">
 import { onMounted } from 'vue'
-import { EstimateGroup } from 'src/features/estimate/api/types'
 import { useEstimateStore } from "src/features/estimate/stores/estimate-store"
 import { useEstimateService } from 'src/features/estimate/composables/estimate-service'
-import EstimateTableGroup from './EstimateTableGroup.vue'
-
 
 const estimateStore = useEstimateStore()
 const estimateService = useEstimateService()
-
-const getKey = (row:EstimateGroup) => {
-  return estimateStore.estimate?.groups.indexOf(row) + '_' + row.type;
-}
 
 onMounted(async() => {
   await estimateService.getEstimate()
@@ -83,7 +84,24 @@ onMounted(async() => {
 
 </script>
 <style scoped>
-.second-row {
-  cursor: pointer;
+.sticky-header-table{
+  max-height: 80vh;
+}
+.sticky-header-table caption{
+  border-bottom:1px solid #e0e0e0 ;
+}
+.sticky-header-table thead tr:first-child th{
+  background-color: white;
+}
+.sticky-header-table thead tr th{
+    text-align: left;
+    position: sticky;
+    z-index: 1;
+}
+.sticky-header-table thead tr:first-child th{
+  top: 0;
+}
+tbody{
+    scroll-margin-top: 48px
 }
 </style>
