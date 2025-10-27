f1f9f072c6607db0dfa9fbbe3e15528467c05881
belchch <rbelchenko@gmail.com>
2025-10-27 20:45:10 +0300

    PRODUCT-64

--- src/features/inspection/components/InspectionPhoto.vue
+++ src/features/inspection/components/InspectionPhoto.vue
@@ -15,15 +15,7 @@ import {usePhotoDocsStore} from "src/features/inspection/store/photo-doc-store";
 import PhotoDocsContainer from "src/features/inspection/components/photo-doc/PhotoDocsContainer.vue";
 import InspectionUnionButton from "src/features/inspection/components/toolbar/InspectionUnionButton.vue";
 import PhotoDocsSkeleton from './photo-doc/PhotoDocsSkeleton.vue';
-import { onMounted } from 'vue';
-import { useMovableSrvice } from 'src/features/measurement/composables/movable';
 
 const { caseLoaded } = storeToRefs(useSelectedCaseStore())
 const { photoDocsLoading, photoDocs } = storeToRefs(usePhotoDocsStore())
-const { requestMovables } = useMovableSrvice()
-
-onMounted(async () => {
-    const inspectionId = 1
-    
-})
 </script>
